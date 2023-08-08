c6cd1552e7cc513b5d935ba1c000ed6526ec7249
FionaZZ92 <fiona.zhao@intel.com>
2023-08-07 22:09:34 -0400

    Enable multiple LoRA weights

--- SD_controlnet/run_pipe.py
+++ SD_controlnet/run_pipe.py
@@ -1,5 +1,5 @@
 from PIL import Image
-from diffusers import UniPCMultistepScheduler, StableDiffusionControlNetPipeline, ControlNetModel
+from diffusers import UniPCMultistepScheduler, EulerAncestralDiscreteScheduler, StableDiffusionControlNetPipeline, ControlNetModel
 import torch
 import numpy as np
 import argparse
@@ -25,38 +25,17 @@ def parse_args() -> argparse.Namespace:
                       help='Specify path of lora weights *.safetensors')
     args.add_argument('-a','--alpha',type = float, default = 0.75, required = False,
                       help='Specify the merging ratio of lora weights, default is 0.75.')
+    args.add_argument('-lp2', '--lora_path2', type = str, default = "", required = False,
+                      help='Specify path of lora weights *.safetensors')
+    args.add_argument('-a2','--alpha2',type = float, default = 0.75, required = False,
+                      help='Specify the merging ratio of lora weights, default is 0.75.')                  
     return parser.parse_args()
 
 def scale_fit_to_window(dst_width:int, dst_height:int, image_width:int, image_height:int):
-    """
-    Preprocessing helper function for calculating image size for resize with peserving original aspect ratio 
-    and fitting image to specific window size
-    
-    Parameters:
-      dst_width (int): destination window width
-      dst_height (int): destination window height
-      image_width (int): source image width
-      image_height (int): source image height
-    Returns:
-      result_width (int): calculated width for resize
-      result_height (int): calculated height for resize
-    """
     im_scale = min(dst_height / image_height, dst_width / image_width)
     return int(im_scale * image_width), int(im_scale * image_height)
 
 def preprocess(image: Image.Image):
-    """
-    Image preprocessing function. Takes image in PIL.Image format, resizes it to keep aspect ration and fits to model input window 512x512,
-    then converts it to np.ndarray and adds padding with zeros on right or bottom side of image (depends from aspect ratio), after that
-    converts data to float32 data type and change range of values from [0, 255] to [-1, 1], finally, converts data layout from planar NHWC to NCHW.
-    The function returns preprocessed input tensor and padding size, which can be used in postprocessing.
-    
-    Parameters:
-      image (Image.Image): input image
-    Returns:
-       image (np.ndarray): preprocessed image tensor
-       pad (Tuple[int]): pading size for each dimension for restoring image size in postprocessing
-    """
     src_width, src_height = image.size
     dst_width, dst_height = scale_fit_to_window(512, 512, src_width, src_height)
     image = np.array(image.resize((dst_width, dst_height), resample=Image.Resampling.LANCZOS))[None, :]
@@ -77,15 +56,6 @@ def randn_tensor(
     shape: Union[Tuple, List],
     dtype: Optional[np.dtype] = np.float32,
 ):
-    """
-    Helper function for generation random values tensor with given shape and data type
-    
-    Parameters:
-      shape (Union[Tuple, List]): shape for filling random values
-      dtype (np.dtype, *optiona*, np.float32): data type for result
-    Returns:
-      latents (np.ndarray): tensor with random values with given data type and shape (usually represents noise in latent space)
-    """
     latents = np.random.randn(*shape).astype(dtype)
 
     return latents
@@ -133,31 +103,18 @@ class OVContrlNetStableDiffusionPipeline(DiffusionPipeline):
         unet: Model,
         vae_decoder: Model,
         state_dict,
-        alpha,
+        alpha_list,
         device:str = "AUTO"
     ):
         super().__init__()
         self.tokenizer = tokenizer
         self.vae_scale_factor = 8 #2 ** (len(self.vae.config.block_out_channels) - 1)
         self.scheduler = scheduler
-        self.load_models(core, device, controlnet, text_encoder, unet, vae_decoder, state_dict, alpha)
+        self.load_models(core, device, controlnet, text_encoder, unet, vae_decoder, state_dict, alpha_list)
         self.set_progress_bar_config(disable=True)
     
 
-    def load_models(self, core: Core, device: str, controlnet:Model, text_encoder: Model, unet: Model, vae_decoder: Model, state_dict, alpha):
-        """
-        Function for loading models on device using OpenVINO
-        
-        Parameters:
-          core (Core): OpenVINO runtime Core class instance
-          device (str): inference device
-          controlnet (Model): OpenVINO Model object represents ControlNet
-          text_encoder (Model): OpenVINO Model object represents text encoder
-          unet (Model): OpenVINO Model object represents UNet
-          vae_decoder (Model): OpenVINO Model object represents vae decoder
-        Returns
-          None
-        """
+    def load_models(self, core: Core, device: str, controlnet:Model, text_encoder: Model, unet: Model, vae_decoder: Model, state_dict, alpha_list):
         if state_dict != None:
             ov_unet = core.read_model(unet)
             ov_text_encoder = core.read_model(text_encoder)
@@ -167,42 +124,51 @@ class OVContrlNetStableDiffusionPipeline(DiffusionPipeline):
             lora_dict_list = []
             LORA_PREFIX_UNET = "lora_unet"
             LORA_PREFIX_TEXT_ENCODER = "lora_te"
+            flag = 0
             manager = Manager()
-            for key in state_dict:
-                if ".alpha" in key or key in visited:
-                    continue
-                if "text" in key:
-                    layer_infos = key.split(LORA_PREFIX_TEXT_ENCODER + "_")[-1].split(".")[0]
-                    lora_dict = dict(name=layer_infos)
-                    lora_dict.update(type="text_encoder")
-                else:
-                    layer_infos = key.split(LORA_PREFIX_UNET + "_")[1].split('.')[0]
-                    lora_dict = dict(name=layer_infos)
-                    lora_dict.update(type="unet")
-                pair_keys = []
-                if "lora_down" in key:
-                    pair_keys.append(key.replace("lora_down", "lora_up"))
-                    pair_keys.append(key)
-                else:
-                    pair_keys.append(key)
-                    pair_keys.append(key.replace("lora_up", "lora_down"))
+            for iter in range(len(state_dict)):
+                visited = []
+                for key in state_dict[iter]:
+                    if ".alpha" in key or key in visited:
+                        continue
+                    if "text" in key:
+                        layer_infos = key.split(LORA_PREFIX_TEXT_ENCODER + "_")[-1].split(".")[0]
+                        lora_dict = dict(name=layer_infos)
+                        lora_dict.update(type="text_encoder")
+                    else:
+                        layer_infos = key.split(LORA_PREFIX_UNET + "_")[1].split('.')[0]
+                        lora_dict = dict(name=layer_infos)
+                        lora_dict.update(type="unet")
+                    pair_keys = []
+                    if "lora_down" in key:
+                        pair_keys.append(key.replace("lora_down", "lora_up"))
+                        pair_keys.append(key)
+                    else:
+                        pair_keys.append(key)
+                        pair_keys.append(key.replace("lora_up", "lora_down"))
 
-                    # update weight
-                if len(state_dict[pair_keys[0]].shape) == 4:
-                    weight_up = state_dict[pair_keys[0]].squeeze(3).squeeze(2).to(torch.float32)
-                    weight_down = state_dict[pair_keys[1]].squeeze(3).squeeze(2).to(torch.float32)
-                    lora_weights = alpha * torch.mm(weight_up, weight_down).unsqueeze(2).unsqueeze(3)
-                    lora_dict.update(value=lora_weights)
-                else:
-                    weight_up = state_dict[pair_keys[0]].to(torch.float32)
-                    weight_down = state_dict[pair_keys[1]].to(torch.float32)
-                    lora_weights = alpha * torch.mm(weight_up, weight_down)
-                    lora_dict.update(value=lora_weights)
-                lora_dict_list.append(lora_dict)
-                # update visited list
-                for item in pair_keys:
-                    visited.append(item)
-
+                        # update weight
+                    if len(state_dict[iter][pair_keys[0]].shape) == 4:
+                        weight_up = state_dict[iter][pair_keys[0]].squeeze(3).squeeze(2).to(torch.float32)
+                        weight_down = state_dict[iter][pair_keys[1]].squeeze(3).squeeze(2).to(torch.float32)
+                        lora_weights = alpha_list[iter] * torch.mm(weight_up, weight_down).unsqueeze(2).unsqueeze(3)
+                        lora_dict.update(value=lora_weights)
+                    else:
+                        weight_up = state_dict[iter][pair_keys[0]].to(torch.float32)
+                        weight_down = state_dict[iter][pair_keys[1]].to(torch.float32)
+                        lora_weights = alpha_list[iter] * torch.mm(weight_up, weight_down)
+                        lora_dict.update(value=lora_weights)
+                    #check if this layer has been appended in lora_dict_list
+                    for ll in lora_dict_list:
+                        if ll["name"] == lora_dict["name"]:
+                            ll["value"] += lora_dict["value"] # all lora weights added together
+                            flag = 1
+                    if flag == 0:
+                        lora_dict_list.append(lora_dict)
+                    # update visited list
+                    for item in pair_keys:
+                        visited.append(item)
+                    flag = 0
             manager.register_pass(InsertLoRA(lora_dict_list))
             if (True in [('type','text_encoder') in l.items() for l in lora_dict_list]):
                 manager.run_passes(ov_text_encoder)
@@ -246,43 +212,10 @@ class OVContrlNetStableDiffusionPipeline(DiffusionPipeline):
         latents: Optional[np.array] = None,
         output_type: Optional[str] = "pil",
     ):
-        """
-        Function invoked when calling the pipeline for generation.
-
-        Parameters:
-            prompt (`str` or `List[str]`):
-                The prompt or prompts to guide the image generation.
-            image (`Image.Image`):
-                `Image`, or tensor representing an image batch which will be repainted according to `prompt`.
-            num_inference_steps (`int`, *optional*, defaults to 100):
-                The number of denoising steps. More denoising steps usually lead to a higher quality image at the
-                expense of slower inference.
-            negative_prompt (`str` or `List[str]`):
-                negative prompt or prompts for generation
-            guidance_scale (`float`, *optional*, defaults to 7.5):
-                Guidance scale as defined in [Classifier-Free Diffusion Guidance](https://arxiv.org/abs/2207.12598).
-                `guidance_scale` is defined as `w` of equation 2. of [Imagen
-                Paper](https://arxiv.org/pdf/2205.11487.pdf). Guidance scale is enabled by setting `guidance_scale >
-                1`. Higher guidance scale encourages to generate images that are closely linked to the text `prompt`,
-                usually at the expense of lower image quality. This pipeline requires a value of at least `1`.
-            latents (`np.ndarray`, *optional*):
-                Pre-generated noisy latents, sampled from a Gaussian distribution, to be used as inputs for image
-                generation. Can be used to tweak the same generation with different prompts. If not provided, a latents
-                tensor will ge generated by sampling using the supplied random `generator`.
-            output_type (`str`, *optional*, defaults to `"pil"`):
-                The output format of the generate image. Choose between
-                [PIL](https://pillow.readthedocs.io/en/stable/): `Image.Image` or `np.array`.
-        Returns:
-            image ([List[Union[np.ndarray, Image.Image]]): generaited images
-            
-        """
 
         # 1. Define call parameters
         batch_size = 1 if isinstance(prompt, str) else len(prompt)
-        #print("batch:",batch_size)
-        # here `guidance_scale` is defined analog to the guidance weight `w` of equation (2)
-        # of the Imagen paper: https://arxiv.org/pdf/2205.11487.pdf . `guidance_scale = 1`
-        # corresponds to doing no classifier free guidance.
+
         do_classifier_free_guidance = guidance_scale > 1.0
         # 2. Encode input prompt
         text_embeddings = self._encode_prompt(prompt, negative_prompt=negative_prompt)
@@ -363,17 +296,6 @@ class OVContrlNetStableDiffusionPipeline(DiffusionPipeline):
         return image
 
     def _encode_prompt(self, prompt:Union[str, List[str]], num_images_per_prompt:int = 1, do_classifier_free_guidance:bool = True, negative_prompt:Union[str, List[str]] = None):
-        """
-        Encodes the prompt into text encoder hidden states.
-
-        Parameters:
-            prompt (str or list(str)): prompt to be encoded
-            num_images_per_prompt (int): number of images that should be generated per prompt
-            do_classifier_free_guidance (bool): whether to use classifier free guidance or not
-            negative_prompt (str or list(str)): negative prompt to be encoded
-        Returns:
-            text_embeddings (np.ndarray): text encoder hidden states
-        """
         batch_size = len(prompt) if isinstance(prompt, list) else 1
 
         # tokenize input prompts
@@ -430,20 +352,7 @@ class OVContrlNetStableDiffusionPipeline(DiffusionPipeline):
         return text_embeddings
 
     def prepare_latents(self, batch_size:int, num_channels_latents:int, height:int, width:int, dtype:np.dtype = np.float32, latents:np.ndarray = None):
-        """
-        Preparing noise to image generation. If initial latents are not provided, they will be generated randomly, 
-        then prepared latents scaled by the standard deviation required by the scheduler
-        
-        Parameters:
-           batch_size (int): input batch size
-           num_channels_latents (int): number of channels for noise generation
-           height (int): image height
-           width (int): image width
-           dtype (np.dtype, *optional*, np.float32): dtype for latents generation
-           latents (np.ndarray, *optional*, None): initial latent noise tensor, if not provided will be generated
-        Returns:
-           latents (np.ndarray): scaled initial noise for diffusion
-        """
+
         shape = (batch_size, num_channels_latents, height // self.vae_scale_factor, width // self.vae_scale_factor)
         if latents is None:
             latents = randn_tensor(shape, dtype=dtype)
@@ -455,15 +364,7 @@ class OVContrlNetStableDiffusionPipeline(DiffusionPipeline):
         return latents
 
     def decode_latents(self, latents:np.array, pad:Tuple[int]):
-        """
-        Decode predicted image from latent space using VAE Decoder and unpad image result
-        
-        Parameters:
-           latents (np.ndarray): image encoded in diffusion latent space
-           pad (Tuple[int]): each side padding sizes obtained on preprocessing step
-        Returns:
-           image: decoded by VAE decoder image
-        """
+
         latents = 1 / 0.18215 * latents # 1 / self.vae.config.scaling_factor * latents
         image = self.vae_decoder(latents)[self.vae_decoder_out]
         (_, end_h), (_, end_w) = pad[1:3]
@@ -481,7 +382,8 @@ pipe = StableDiffusionControlNetPipeline.from_pretrained("stable-diffusion-v1-5"
 
 tokenizer = CLIPTokenizer.from_pretrained('clip-vit-large-patch14')
 scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
-#print(pipe.scheduler.config)
+#scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+
 CONTROLNET_OV_PATH = "controlnet-canny.xml"
 TEXT_ENCODER_OV_PATH = "text_encoder.xml"
 UNET_OV_PATH = "unet_controlnet.xml"
@@ -490,18 +392,31 @@ VAE_DECODER_OV_PATH = "vae_decoder.xml"
 core = Core()
 #core.set_property({'CACHE_DIR': './cache'})
 #====Add lora======
-LORA_PATH = args.lora_path
+LORA_PATH = []
+LORA_ALPHA = []
+if args.lora_path != "":
+    #lora_pair = dict(path=args.lora_path)
+    #lora_pair.update(alpha=args.alpha)
+    LORA_PATH.append(args.lora_path)
+    LORA_ALPHA.append(args.alpha)
+    if args.lora_path2 != "":
+        #lora_pair = dict(path=args.lora_path2)
+        #lora_pair.update(alpha=args.alpha2)
+        LORA_PATH.append(args.lora_path2)
+        LORA_ALPHA.append(args.alpha)
+
+state_dict = []
 # load LoRA weight from .safetensors
-if LORA_PATH == "":
-    ov_pipe = OVContrlNetStableDiffusionPipeline(tokenizer, scheduler, core, CONTROLNET_OV_PATH, TEXT_ENCODER_OV_PATH, UNET_OV_PATH, VAE_DECODER_OV_PATH, None, args.alpha, device="AUTO") #change to CPU or GPU
+if len(LORA_PATH) == 0:
+    ov_pipe = OVContrlNetStableDiffusionPipeline(tokenizer, scheduler, core, CONTROLNET_OV_PATH, TEXT_ENCODER_OV_PATH, UNET_OV_PATH, VAE_DECODER_OV_PATH, None, None, device="AUTO") #change to CPU or GPU
 else:
-    state_dict = load_file(LORA_PATH)
-    ov_pipe = OVContrlNetStableDiffusionPipeline(tokenizer, scheduler, core, CONTROLNET_OV_PATH, TEXT_ENCODER_OV_PATH, UNET_OV_PATH, VAE_DECODER_OV_PATH, state_dict, args.alpha, device="AUTO") #change to CPU or GPU
+    [state_dict.append(load_file(p)) for p in LORA_PATH] #state_dict is list of lora list
+    ov_pipe = OVContrlNetStableDiffusionPipeline(tokenizer, scheduler, core, CONTROLNET_OV_PATH, TEXT_ENCODER_OV_PATH, UNET_OV_PATH, VAE_DECODER_OV_PATH, state_dict, LORA_ALPHA, device="AUTO") #change to CPU or GPU
 
 image = load_image("vermeer_512x512.png")
 image = np.array(image)
 
-low_threshold = 100
+low_threshold = 150
 high_threshold = 200
 
 image = cv2.Canny(image, low_threshold, high_threshold)
@@ -510,11 +425,13 @@ image = np.concatenate([image, image, image], axis=2)
 image = Image.fromarray(image)
 #image.save("canny.png")
 
-prompt = ["Girl with Pearl Earring","Gril with blue cloth"]
-#prompt = ["Girl with Pearl Earring"]
+prompt = ["Girl with Pearl Earring","Girl with blue cloth"]
+#prompt = ["Girl with Pearl Earring","1girl, cute, beautiful face, portrait, cloudy mountain, outdoors, trees, rock, river, (soul card:1.2), highly intricate details, realistic light, trending on cgsociety,neon details, ultra realistic details, global illumination, shadows, octane render, 8k, ultra sharp"]
 num_steps = 20
-negative_prompt = ["monochrome, lowres, bad anatomy, worst quality, low quality","monochrome, lowres, bad anatomy, worst quality, low quality"]
-#negative_prompt = ["monochrome, lowres, bad anatomy, worst quality, low quality"] #use single batch
+
+negtive_prompt = ["monochrome, lowres, bad anatomy, worst quality, low quality","monochrome, lowres, bad anatomy, worst quality, low quality"]
+#negative_prompt = ["monochrome, lowres, bad anatomy, worst quality, low quality","3d, cartoon, lowres, bad anatomy, bad hands, text, error"]
+
 
 np.random.seed(42)
 start = time.time()
